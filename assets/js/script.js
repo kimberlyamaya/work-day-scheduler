daef98c51cfc9e1cce427f1d09f8858888a2188e
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-11-02 14:31:51 -0700

    add timeblock

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@ var todayDate = moment().format("dddd, MMMM Do, YYYY");
 //console.log(rightNow);
 
 $("#currentDay") .text(todayDate);  
+
+// create the input field
+
+// append the input field into
